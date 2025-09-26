4202080bc154b064ecfa0be21b48f1eeb744f3b7
gustavorsaul <gustavo.saul@edu.pucrs.br>
2025-09-26 17:40:55 -0300

    criação de Player, Tutorial e testando sprites e tiles

---
+++ Player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+@export var speed = 300.0
+
+@onready var sprite = $AnimatedSprite2D
+
+@export var arrow : PackedScene
+
+func get_8way_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	
+func animate():
+	if velocity.x > 0:
+		sprite.play("right")
+	elif velocity.x < 0:
+		sprite.play("left")
+	elif velocity.y > 0:
+		sprite.play("down")
+	elif velocity.y < 0:
+		sprite.play("up")
+	else:
+		sprite.stop()
+		
+func move_8way(delta):
+	get_8way_input()
+	animate()
+	move_and_slide()
+
+func _physics_process(delta):
+	move_8way(delta)
+	
+	if (Input.is_action_just_pressed("mouseLeft")):
+		print("Shoot")
+		var b := bullet.instantiate()
+		b.position = position
+		owner.add_child(b)
